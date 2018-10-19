23a97ba8ce3f578e03e43c6797fd570149d613e4
Derek L <derekslarson@gmail.com>
2018-10-19 09:30:20 +0700

    dumb bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Graph from './components/Graph';
-import Test from './components/Test'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-       <Test />
+       <Graph />
     );
   }
 }

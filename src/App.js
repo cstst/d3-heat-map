119c2b6625705fd1dbdfb52fd9f90a1c9aa876db
Derek L <derekslarson@gmail.com>
2018-10-19 09:19:56 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Graph from './components/Graph';
+import Test from './components/Test'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-       <Graph />
+       <Test />
     );
   }
 }

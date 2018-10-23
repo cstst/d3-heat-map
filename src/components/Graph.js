2cb1912462ed6ca5e4c3e99a460bce05b435b63a
Derek L <derekslarson@gmail.com>
2018-10-23 18:04:57 +0700

    scale fixed

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -12,9 +12,10 @@ export default class Graph extends Component {
   drawHeatMap({ monthlyVariance, baseTemperature }) {
     const height = 600,
           width = 1500,
-          margin = {top: 60, right: 30, bottom: 100, left: 60},
+          margin = {top: 60, right: 30, bottom: 150, left: 60},
           years = monthlyVariance.map(yearData => yearData.year),
           temps = monthlyVariance.map(yearData => yearData.variance),
+          tempDif = (d3.max(temps) - d3.min(temps)) / 11,
           svg = d3.select('#graph')
                   .append('svg')
                   .attr('height', height)
@@ -52,7 +53,7 @@ export default class Graph extends Component {
        .attr('month', d => monthScale(d.month))
        .style('fill', d => colorScale(d.variance))
        .on('mouseover', (d, i) => {
-          tooltip.text(`${monthScale(d.month)} ${d.year}\n${(Math.round((baseTemperature + d.variance) * 10) / 10)}℃\n${(Math.round(d.variance * 10) / 10)}℃`)
+          tooltip.text(`${monthScale(d.month)} ${d.year}\n${(Math.round((baseTemperature + d.variance) * 10) / 10)}°C\n${d.variance > 0 ? '+' : ''}${(Math.round(d.variance * 10) / 10)}°C`)
                  .style("visibility", "visible")
         })
       .on("mousemove", () => {
@@ -84,36 +85,45 @@ export default class Graph extends Component {
        .attr('y', margin.top / 1.2)
        .attr('id', 'subtitle')
        .text('1753-2015: Base Temperature 8.66℃');
-      
-    const tempDif = (d3.max(temps) - d3.min(temps)) / 12,
-          tempTicks = [];
-    for (let i = 0; i < 12; i++) {
-      tempTicks.push(d3.min(temps) + tempDif * i)
+    
+    const legendColorVals = [],
+          legendTickVals = [];
+    for (let i = 0; i < 11; i++) {
+      legendColorVals.push(d3.min(temps) + ((tempDif * i) + (tempDif / 2)));
+      if (i > 0) {
+        legendTickVals.push(d3.min(temps) + (tempDif * i));
+      }   
     }
-    console.log(tempTicks)
-    const legend = svg.selectAll('.legend')
-       .data(tempTicks)
-       .enter()
-       .append('g')
-       .attr('class', 'legend')
-       .attr('id', 'legend')
-       .attr('transform', (d, i) => `translate(${i * 30}, 0)`);
+    console.log(legendTickVals)
+    const legend = svg.append('g')
+                      .attr('id', 'legend')
 
-    legend.append('rect')
-          .attr('x', margin.left)
+    legend.selectAll('rect')
+          .data(legendColorVals)
+          .enter()
+          .append('rect')
+          .attr('x', (d, i) => margin.left + (40 * i))
           .attr('y', height - (margin.bottom / 2))
+          .attr('val', d => d)
           .attr('class', 'legend-rect')
-          .attr('width', 30)
-          .attr('height', 30)
+          .attr('width', 40)
+          .attr('height', 40)
           .style('fill', d => colorScale(d));
-/*
-    legend.append('text')
-          .attr('x', width - 24)
-          .attr('y', 9)
-          .attr('dy', '.35em')
-          .style('text-anchor', 'end')
-          .text(d => d ? 'Riders with doping allegations' : 'No doping allegations');
-  */                
+    
+    const legendScale = d3.scaleLinear()
+                          .domain([legendTickVals[0], legendTickVals[legendTickVals.length - 1]])
+                          .range([margin.left + 39, margin.left + 399]);
+
+    const legendAxis = d3.axisBottom(legendScale)
+                         .tickValues(legendTickVals)
+                         .tickFormat( n => (n > 0 ? '+' : '') + (Math.round(n * 10) / 10) + '°');    
+    legend.append("g")
+          .attr('id', 'legend-axis')
+          .attr("transform", `translate(0, ${height - (margin.bottom / 2) + 39})`)
+          .call(legendAxis);
+    
+
+
   }
   
   render() {
